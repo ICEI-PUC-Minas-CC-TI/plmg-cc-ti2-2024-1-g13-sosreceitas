b337610246c0f0095d85c896484072722145d3e8
Lucas Soares Tavares <130789583+tavaresslc@users.noreply.github.com>
2024-05-03 10:50:56 -0300

    Update profile/script.js

--- Codigo/SOS_Receitas/src/profile/script.js
+++ Codigo/SOS_Receitas/src/profile/script.js
@@ -1,8 +1,25 @@
+userLogado = JSON.parse(localStorage.getItem("userLogado"));
+const userName = document.getElementById("userName");
+userName.innerHTML = userLogado.nome;
+
+const editBtn = document.getElementById("edit-btn");
+editBtn.addEventListener("click", () => {
+  const novoNome = prompt("Digite o novo nome:");
+
+  if (novoNome !== null && novoNome.trim() !== "") {
+    userLogado.nome = novoNome;
+    userName.innerHTML = userLogado.nome;
+    localStorage.setItem("userLogado", JSON.stringify(userLogado));
+  }
+});
+
 const userRecipes = [
   { name: "Bolo de Chocolate", likes: 25 },
   { name: "Lasanha", likes: 30 },
   { name: "Frango Assado", likes: 15 },
 ];
+
+
 
 function renderRecipes() {
   const recipesList = document.getElementById("recipes");
@@ -18,26 +35,26 @@ function renderRecipes() {
 renderRecipes();
 
 document.getElementById("logout-btn").addEventListener("click", () => {
-  alert("Saindo da conta...");
-  window.location.href = '../../index.html'
+  const confirmacao = confirm("Deseja realmente sair?");
+  if (confirmacao) {
+    setTimeout(() => {
+      window.location.href = "../../index.html";
+    }, 1000);
+    localStorage.removeItem("userLogado");
+  }
 });
 
-document.getElementById("home-btn").addEventListener("click", () => {
-  alert("Voltando para a tela inicial...");
-  window.location.href = '../chat/index.html'
+document.getElementById("go-back").addEventListener("click", () => {
+  window.location.href = "../chat/index.html";
 });
 
 document
   .getElementById("add-recipe-btn")
-  .addEventListener("click", function () {
-    alert("Redirecionar para a página de formulário de receita...");
-  });
+  .addEventListener("click", function () {});
 
 function adjustAddRecipeButtonPosition() {
   const recipesList = document.querySelector(".recipes-list");
   const addRecipeButton = document.getElementById("add-recipe-btn");
-
-  addRecipeButton.style.marginTop = `${recipesList.offsetHeight}px`;
 }
 
 window.addEventListener("load", adjustAddRecipeButtonPosition);
